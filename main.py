a2c54181df8666cf9e52368bcd0dd885dff9c42a
Nenisoft <nenisoft@Nenisoft-MacBook-Pro.local>
2023-02-28 20:47:54 +0100

    Changed the check result from a list to dictionary. implemented anlysis of password

--- main.py
+++ main.py
@@ -1,43 +1,61 @@
-checkResult = []
+# list to store the result of the checks
+checkResult = {}
 
 
+# function to check the length of the password
 def checkLength(pwd):
     if len(pwd) >= 8:
-        checkResult.append(True)
+        checkResult["length"] = True
     else:
-        checkResult.append(False)
+        checkResult["length"] = False
 
 
+# function to check if the password contains a number
 def checkDigit(pwd):
     digit = False
     for i in pwd:
         if i.isdigit():
             digit = True
 
-    checkResult.append(digit)
+    checkResult["digit"] = digit
 
 
+# function to check if the password contains an uppercase character
 def checkUpper(pwd):
     upperCase = False
     for i in pwd:
         if i.isupper():
             upperCase = True
-    checkResult.append(upperCase)
+    checkResult["upper"] = upperCase
 
 
+# Get the user input
 password = input("Enter Password: ")
+
+# call the three functions above to check
 checkLength(password)
 checkDigit(password)
 checkUpper(password)
 
-pwd_true = checkResult.count(True)
-pwd_strength = round(pwd_true / len(checkResult) * 100)
-if pwd_strength >= 90:
-    print(f"Password Strength: {pwd_strength}% ... Strong Password")
-elif pwd_strength >= 70:
-    print(f"Password Strength: {pwd_strength}% ... Fair Password")
-elif pwd_strength >= 50:
-    print(f"Password Strength: {pwd_strength}% ... Poor Password")
-else:
-    print(f"Password Strength: {pwd_strength}% ... Weak Password")
+# Check the list and count the occurrence of TRUE
+pwd_true = 0
+for value in checkResult.values():
+    if value:
+        pwd_true += 1
 
+# Calculate the percentage of truth which will be used as the password strength
+pwd_strength = round(pwd_true / len(checkResult.values()) * 100)
+
+# Print a message and display the strength based on its value.
+print(f"==== Password Analysis ==== \nLength: {len(password)}\n"
+      f"Numbers: {checkResult['digit']}\n"
+      f"UpperCase: {checkResult['upper']}\n"
+      f"Strength: {pwd_strength}%")
+if pwd_strength >= 90:
+    print("Conclusion: Strong Password")
+elif pwd_strength >= 70:
+    print("Conclusion: Fair Password")
+elif pwd_strength >= 50:
+    print("Conclusion: Poor Password")
+else:
+    print("Conclusion: Weak Password")
